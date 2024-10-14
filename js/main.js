c903f83845eae9d2a6f31b5d358c897c9491561c
ASHISH-RATNA-DARLA <darlaashishratna@gmail.com>
2024-10-14 17:27:22 +0530

    Initial commit with new history

---
+++ js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll', function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if ($(window).scrollTop() > 35) {
+            $('.header').css({'background':'#002e5f', 'box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+        } else {
+            $('.header').css({'background':'none', 'box-shadow':'none'});
+        }
+    });
+
+    const counters = document.querySelectorAll('.counter');
+    const speed = 120;
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const inc = target / speed;
+            if (count < target) {
+                counter.innerText = count + inc;
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+
+    (function ($) {
+        "use strict";
+
+        $(".clients-carousel").owlCarousel({
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
+        });
+
+        $(".testimonials-carousel").owlCarousel({
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
+        });
+
+    })(jQuery);
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    $('.accordion-header').click(function(){
+        $('.accordion .accordion-body').slideUp(500);
+        $(this).next('.accordion-body').slideDown(500);
+        $('.accordion .accordion-header span').text('+');
+        $(this).children('span').text('-');
+    });
+
+    // Toggle More/Less Text
+    function toggleText() {
+        var moreText = document.getElementById("more-text");
+        var btnText = document.querySelector(".btn");
+
+        if (moreText.style.display === "none") {
+            moreText.style.display = "inline";
+            btnText.textContent = "Read Less";
+        } else {
+            moreText.style.display = "none";
+            btnText.textContent = "Read More";
+        }
+    }
+
+    // Event Listener for Read More Button
+    $('.btn').click(toggleText);
+
+});
